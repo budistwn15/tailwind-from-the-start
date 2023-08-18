f8bbfe814fb8773402c3807933a89aac7be5a4e3
budistwn15 <budi.setiawan112001@gmail.com>
2023-08-18 22:44:32 +0700

    Let's create the routing first

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { IconLogout } from "@tabler/icons-react";
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Home from './views/Home';
 
-export default function App() {
-
-  return (
-    <div className="antialiased font-sans">
-        <div className="flex items-center justify-center h-screen">
-            <button className="bg-blue-500 hover:bg-blue-600 font-medium px-4 py-2.5 rounded text-white inline-flex items-center transition-colors duration-200">
-                <IconLogout className="mr-2"/>
-                Button
-            </button>
-        </div>
-    </div>
-  )
+function app() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home/>} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
+
+export default app;
