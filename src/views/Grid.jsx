1b1f6c1fdfaec0986685d3534ac90c0954d067fa
budistwn15 <budi.setiawan112001@gmail.com>
2023-08-19 22:54:22 +0700

    kita buat responsive untuk grid

--- src/views/Grid.jsx
+++ src/views/Grid.jsx
@@ -2,25 +2,13 @@ export default function Grid() {
     return (
         <div className="antialiased font-sans">
             <div className="container mx-auto py-10">
-                <div className="grid grid-cols-3 gap-4">
-                    <div className="col-span-2">
-                        <div className="bg-gray-800 text-white py-10">Column 1</div>
-                    </div>
-                    <div>
-                        <div className="bg-gray-800 text-white py-10">Column 2</div>
-                    </div>
-                    <div>
-                        <div className="bg-gray-800 text-white py-10">Column 3</div>
-                    </div>
-                    <div>
-                        <div className="bg-gray-800 text-white py-10">Column 1</div>
-                    </div>
-                    <div>
-                        <div className="bg-gray-800 text-white py-10">Column 2</div>
-                    </div>
-                    <div>
-                        <div className="bg-gray-800 text-white py-10">Column 3</div>
-                    </div>
+                <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+                    <div className="bg-gray-500">&nbsp;</div>
+                    <div className="bg-gray-500">&nbsp;</div>
+                    <div className="bg-gray-500">&nbsp;</div>
+                    <div className="bg-gray-500">&nbsp;</div>
+                    <div className="bg-gray-500">&nbsp;</div>
+                    <div className="bg-gray-500 lg:bg-teal-500">&nbsp;</div>
                 </div>
             </div>
         </div>
